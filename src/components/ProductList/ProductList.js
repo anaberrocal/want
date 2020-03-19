b73fac1905afc1e79025ebcd6f2c63ce1d5da8cd
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-18 21:00:39 -0400

    Add footer responsiveness

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -8,17 +8,22 @@ import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
 import Product from '../Product/Product.js';
 import MediaCard from '../Product/Product.js'
+import './ProductList.js';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    height: 300,
-    width: 300,
-  },
+  // paper: {
+  //   height: 300,
+  //   width: 300,
+  // },
   control: {
     padding: theme.spacing(2),
+  },
+  mediaCard: {
+    height: 300,
+    width: 300,
   },
 }));
 
@@ -28,13 +33,13 @@ export default function SpacingGrid() {
 
 
   return (
-    <Grid container className={classes.root} spacing={2}>
+    <Grid id="container" container className={classes.root} spacing={2}>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
-          {[0, 1, 2].map(value => (
-            <Grid key={value} item>
-              <Paper className={classes.paper} />
-              <MediaCard/>
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(value => (
+            <Grid key={value} item className={classes.mediaCard}>
+              {/* <Paper className={classes.paper} /> */}
+              <MediaCard />
             </Grid>
           ))}
         </Grid>
