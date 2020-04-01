498539d9ff40545deb76443e70c3a5c678fb9f88
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-31 20:21:50 -0400

    Update product array

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -28,9 +28,9 @@ const product = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet vulputate arcu. Nam auctor urna nec dui pretium, vitae pharetra erat sodales. Maecenas gravida, metus tincidunt scelerisque feugiat, nisl dui porttitor enim, sed mattis erat augue non ante."
   },
   {
-    productName: "4PCS Wireless Bluetooth Earphone",
-    productPrice: "$10.00",
-    productImg: "https://contestimg.wish.com/api/image/fetch?contest_id=5c4a034e9ed6dd0c7d18f6ec&s=0&w=600&h=600&q=80",
+    productName: "Tricycle Bike Flower Basket",
+    productPrice: "$4.00",
+    productImg: "https://contestimg.wish.com/api/image/fetch?contest_id=5bb09f17a90b244d2642e7b5&s=10&w=600&h=600&q=80",
     ProductDescription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet vulputate arcu. Nam auctor urna nec dui pretium, vitae pharetra erat sodales. Maecenas gravida, metus tincidunt scelerisque feugiat, nisl dui porttitor enim, sed mattis erat augue non ante."
   },
